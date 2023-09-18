49bacaace2a9e6bfa79d3cf100af0e8bf737506b
AbylayBoztay <abylai_boztai@mail.ru>
2023-09-18 20:11:58 +0600

    weatherapp is ready

---
+++ src/pages/SecondPage.jsx
@@ -0,0 +1,23 @@
+import React from "react"
+
+
+export default function SecondPage(){
+    return (
+        <>
+            <div>
+                <h3>Это страница, созданная с помощью React-router (v.6). Стек технологии, которые были использованы в ходе создания веб приложения "todo-list" : </h3>
+            </div>
+            <div>
+                <nav>
+                    <ul>
+                        <li>-react(v.18)</li>
+                        <li>-react-router (v.6)</li>
+                        <li>-vite/webpack</li>
+                        <li>-ui babel</li>
+                        <li>-JavaScript</li>
+                    </ul>
+                </nav>
+            </div>
+        </>
+    )
+}
